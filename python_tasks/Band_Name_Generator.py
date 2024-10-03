1c28cd028b78469a8b2303bac53f8d44e58abbc8
Priyanka <payyavulapriyanka5@gmail.com>
2024-10-03 10:46:52 +0200

    oct 2 changes

---
+++ python_tasks/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator")
+city=input("What's the name of your city you grew up in?\n")
+pet_name=input("What is your pet name?\n")
+
+print("Your Band name could be " + city + " " +pet_name)
--- python_tasks/test.py
+++ python_tasks/test.py
@@ -1,1 +1,5 @@
-print('Hello')
+print('Hello world!')
+
+Username="priya"
+length=len(Username)
+print(length)
